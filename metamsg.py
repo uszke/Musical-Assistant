873c8484d2e16cdf285141119a0721fd61ee0b0f
boczkovsky <36506606+boczkovsky@users.noreply.github.com>
2019-05-30 18:06:15 +0200

    Update metamsg.py

--- metamsg.py
+++ metamsg.py
@@ -26,29 +26,3 @@ for i, track in enumerate(mid.tracks):
 		hit()
 		time.sleep(msg.time)
                 print('hit', msg.time)
-
-=======
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
-
-track.append(Message('program_change', program=12, time=0))
-track.append(Message('note_on', note=30, velocity=64, time=0))
-track.append(Message('note_off', note=30, velocity=127, time=60))
-track.append(Message('note_on', note=70, velocity=64, time=120))
-track.append(Message('note_off', note=70, velocity=127, time=120))
-track.append(Message('note_on', note=65, velocity=64, time=180))
-track.append(Message('note_off', note=65, velocity=127, time=240))
-
-mid.save('tests.mid')
-
-for i, track in enumerate(mid.tracks):
-    print('Track{}:{}'.format(i, track.name))
-    for msg in track:
-        # if msg == 'note=30, velocity=127, time=60':
-        #	print(msg)
-        # if msg.is_meta :
-        # print(msg.bytes())
-        # L = msg.bytes()
-        print(msg.bytes()[2:])
->>>>>>> 7e428f6dd71d8c221e4f0e221ee09bed517bf8df
