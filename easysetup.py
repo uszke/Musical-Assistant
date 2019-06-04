96ee58c0dd843d274104e9e756b749df933a18c7
Mateusz <mateusz.trojca@gmail.com>
2019-06-04 11:08:34 +0200

    Re-write pt.2

--- easysetup.py
+++ easysetup.py
@@ -3,40 +3,60 @@ import time
 from RPi import GPIO
 from time import sleep
 
+motor1 = 0
+motor2 = 0
+note1 = 0
+note2 = 0
+
 GPIO.VERBOSE = False
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-name = str(input("How many motors are you using: "))
+name = int(input("How many motors are you using: "))
 
 if name == 1:
     motor1 = 23
-    note1 = str(input("What note it refers to: "))
+    note1 = int(input("What note it refers to: "))
 elif name == 2:
+    motor1 = 23
+    note1 = int(input("What note first motor refers to: "))
     motor2 = 24
-    note2 = str(input("What note second motor refers to: "))
+    note2 = int(input("What note second motor refers to: "))
 else:
     print("I'm just a prototype! Please be easy on me! :( ")
 
 GPIO.setup(motor1, GPIO.OUT)
-GPIO.setup(motor2, GPIO,OUT)
+GPIO.setup(motor2, GPIO.OUT)
 
 
-def hit():
-    GPIO.output(23, GPIO.HIGH)
+def hit_motor1():
+    GPIO.output(motor1, GPIO.HIGH)
     sleep(0.07)
     print("ON")
-    GPIO.output(23, GPIO.LOW)
+    GPIO.output(motor1, GPIO.LOW)
     sleep(0.5)
     print("OFF")
+
+
+def hit_motor2():
+    GPIO.output(motor2, GPIO.HIGH)
+    sleep(0.07)
+    print("Second motor ON")
+    GPIO.output(motor2, GPIO.LOW)
+    sleep(0.5)
+    print("Second motor OFF")
 
 
 mid = MidiFile('tadow.mp3.mid')
 for i, track in enumerate(mid.tracks):
     print('Track{}:{}'.format(i, track.name))
     for msg in track:
-        list = msg.bytes()
-        if tuple(list)[-2] == 26:
-            hit()
+        list1 = msg.bytes()
+        if tuple(list1)[-2] == 26:
+            hit_motor1()
             time.sleep(msg.time)
-            print('hit', msg.time)
+            print('Motor 1 hit ', msg.time)
+        if tuple(list1)[-2] == 133:
+            hit_motor2()
+            time.sleep(msg.time)
+            print("Motor 2 hit", msg.time)
--- readmidi.py
+++ readmidi.py
@@ -1,14 +1,16 @@
 from mido import MidiFile, tempo2bpm
 from pyasn1.type import char
 
-mid =  MidiFile('tadow.mp3.mid')
-char x = 'note_on channel=0 note=51 velocity=67 time=1'
+mid = MidiFile('tadow.mp3.mid')
+x = 'note_on channel=0 note=51 velocity=67 time=1'
 for i, track in enumerate(mid.tracks):
-	print('Track{}:{}'.format(i,track.name))
-	for msg in track :
-	#	if msg != msg.is_meta:
-			
-		if msg !=msg.is_meta  and  msg.type is x:
-			print(msg)
-	#	else :
-	#		print msg
+    print('Track{}:{}'.format(i, track.name))
+    for msg in track:
+        #	if msg != msg.is_meta:
+
+      #  if msg != msg.is_meta and msg.type is x:
+        print(msg.bytes())
+#	else :
+#		print msg
+
+
